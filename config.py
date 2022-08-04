48db0992269db74e2379238a52dcbde6bba0a3ed
unknown <shyamkokare1385@gmail.com>
2022-08-04 23:07:25 +0530

    added API file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = 'enter_path'
+
+PORT_NUMBER = 5005
+
+MODEL_FILE_PATH = os.path.join('project_app','linear_regression_model.pkl')
+
+JSON_FILE_PATH = os.path.join('project_app','project_data.json')
